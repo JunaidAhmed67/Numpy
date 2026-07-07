326ebaecad783e05b2bb4e2b7338f428060d68ce
JunaidAhmed68 <jb7303721@gmail.com>
2026-07-07 13:32:16 +0500

    creating arry completes.

---
+++ Creating_Array/Numpy_Day1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+# num_list =[ 2,3,4,5,6,7,89,8]
+# print(num_list)
+
+# numpy_array= np.array(num_list)
+# print(numpy_array)
+
+#  1D array is simple list of value
+array = [1,2,3,4,5]
+numpy_array= np.array(array)
+print(numpy_array)
+
+#  2D array is row w columns
+# array = [[1,2,3,4,5],[2,5,7,9,4],[5,4,3,2,1]]
+numpy_array= np.array([[1,2,3,4,5],
+                      [2,5,7,9,4],
+                      [5,4,3,2,1]])
+print(numpy_array)
+
+#  multiD array is 2 or more D array / matrix
+# array = [[1,2,3,4,5],[2,5,7,9,4],[5,4,3,2,1]]
+numpy_array= np.array([[1,2,3,4,5],
+                      [2,5,7,9,4],
+                      [5,4,3,2,1]])
+print(numpy_array)
+
---
+++ Creating_Array/Numpy_Day2.py
@@ -0,0 +1,80 @@
+import numpy as np 
+# zeros_array = np.zeros((2,3))
+# print(zeros_array)
+
+
+# zeros_array = np.ones(3)
+# print(zeros_array)
+
+# full_array = np.full((2,2),6)
+# print(full_array)
+
+# arr_arrange = np.arange(3,12,2)
+# print(arr_arrange)
+
+
+
+
+
+
+
+
+# Q1 [5, 10, 15, 20]
+arr = np.arange(5,21,5)
+# print(arr)
+
+# Q2 Create a 4×4 array of zeros.
+arr1 = np.zeros((4,4))
+# print(arr1)
+
+# Q3 Create a 3×5 array of ones.
+arr3 = np.ones((3,5))
+# print(arr3)
+
+# Q4 Create a 2×6 array filled with 9.
+arr4 = np.full((2,6),9)
+# print(arr4)
+
+# Q5 create [1 2 3 4 5 6 7 8 9 10]
+arr5 = np.arange(1,11)
+# print(arr5)
+
+# Q6 create [10 20 30 40 50]
+arr6 = np.arange(10,51,10)
+# print(arr6)
+
+# Q7 Create all even numbers from 2 to 20.
+arr7 = np.arange(2,21,2)
+# print(arr7)
+
+# Q8 Create all odd numbers from 1 to 19.
+arr8 = np.arange(1,20,2)
+# print(arr8)
+
+# Q9 Create numbers from 100 to 0.
+arr9 = np.arange(100,-1,-10)
+# print(arr9)
+
+# Q10 Create a 5×5 identity matrix.
+arr10 = np.eye(5,5)
+# print(arr10)
+
+# Q11 Create a 6×6 matrix filled with 7.
+arr11 = np.full((6,6),7)
+# print(arr11)
+
+# Q12 create [50 51 52 53 54 55]
+arr12 = np.arange(50,56)
+# print(arr12)
+
+# Q13 create [-5 -4 -3 -2 -1 0 1 2 3 4 5]
+arr13 = np.arange(-5,6)
+# print(arr13)
+
+# Q14 Create a 10×10 matrix filled with 100.
+arr14 = np.full((10,10),100)
+# print(arr14)
+
+# Q15 Create a 7×7 identity matrix.
+arr15 = np.eye(7)
+print(arr15)
